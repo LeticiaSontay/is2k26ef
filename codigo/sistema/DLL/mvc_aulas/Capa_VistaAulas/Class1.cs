82ad84ad5c545a770ddce7dc9dc0628ff18ec6bd
Leticia Sontay <lsontayc1@miumg.edu.gt>
2026-05-28 10:47:26 -0600

    Se crea manetnimiento de Aulas, se agrega referencia de navegador y se agrega codigo de navegador con la tabla aulas

---
+++ codigo/sistema/DLL/mvc_aulas/Capa_VistaAulas/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_VistaAulas
+{
+    public class Class1
+    {
+    }
+}
---
+++ codigo/sistema/DLL/mvc_aulas/mvc_aulas/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvc_aulas
+{
+    class Class1
+    {
+    }
+}
